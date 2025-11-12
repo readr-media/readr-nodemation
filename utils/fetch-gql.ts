836344ab0f4c8529ca3ebb38753b9e877c670f67
LinHeMa <linhema0921@gmail.com>
2025-11-12 11:29:34 +0800

    feat: setup gql codegen flow

---
+++ utils/fetch-gql.ts
@@ -0,0 +1,73 @@
+import type { OperationVariables } from "@apollo/client";
+import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
+
+import { getClient } from "@/lib/client";
+import { logServerSideError, type TraceObject } from "./log";
+
+export default async function queryGraphQL<
+  TResult,
+  TVariables extends OperationVariables
+>(
+  query: TypedDocumentNode<TResult, TVariables>,
+  variables?: TVariables,
+  traceObject?: TraceObject,
+  errorMessage?: string
+): Promise<TResult | null> {
+  try {
+    const { data, errors: gqlErrors } = await getClient().query({
+      query,
+      variables,
+    });
+
+    if (gqlErrors && gqlErrors.length > 0) {
+      throw new Error(`[GraphQL error]: ${gqlErrors[0].message}`);
+    }
+    return data;
+  } catch (error) {
+    const fallbackErrorMessage = `Fetch GraphQL failed, info: ${JSON.stringify({
+      query,
+      variables,
+    })}`;
+
+    logServerSideError(
+      error,
+      errorMessage || fallbackErrorMessage,
+      traceObject
+    );
+    return null;
+  }
+}
+
+export async function mutateGraphQL<
+  TResult,
+  TVariables extends OperationVariables
+>(
+  mutation: TypedDocumentNode<TResult, TVariables>,
+  variables?: TVariables,
+  traceObject?: TraceObject,
+  errorMessage?: string
+): Promise<TResult | null> {
+  try {
+    const { data, errors: gqlErrors } = await getClient().mutate({
+      mutation,
+      variables,
+    });
+    if (gqlErrors && gqlErrors.length > 0) {
+      throw new Error(`[GraphQL error]: ${gqlErrors[0].message}`);
+    }
+    return data || null;
+  } catch (error) {
+    const fallbackErrorMessage = `Upload GraphQL failed, info: ${JSON.stringify(
+      {
+        mutation,
+        variables,
+      }
+    )}`;
+    logServerSideError(
+      error,
+      errorMessage || fallbackErrorMessage,
+      traceObject
+    );
+    return null;
+  }
+}

3401cbb785fc3a75310190b9bbf54b6af8922c99
LinHeMa <linhema0921@gmail.com>
2025-12-11 10:05:12 +0800

    feat: ModuleCard UI
    feat: remove dup

--- app/[workflow-builder]/components/module-sidebar.tsx
+++ app/[workflow-builder]/components/module-sidebar.tsx
@@ -1,9 +1,11 @@
+import ModuleList from "./module-list";
 import ModuleSearch from "./module-search";
 
 const ModuleSideBar = () => {
   return (
     <div className="shadow-gray-400 shadow-sm min-w-3xs flex flex-col">
       <ModuleSearch />
+      <ModuleList />
     </div>
   );
 };

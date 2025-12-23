097ff8f21d3c99027845aa422c80f786bd1d8526
nihclil <oxx921@gmail.com>
2025-12-23 08:16:25 +0800

    feat: add header on workflow page

--- app/[workflow-builder]/layout.tsx
+++ app/[workflow-builder]/layout.tsx
@@ -1,16 +1,20 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import ModuleSideBar from "./components/module-sidebar";
 import NodeSettingSidebar from "./components/node-setting-sidebar";
+import Header from "./components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider defaultOpen={false}>
-      <ModuleSideBar />
-      <main>
-        <SidebarTrigger triggerVariant="floating" />
-        {children}
-      </main>
-      <NodeSettingSidebar />
-    </SidebarProvider>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <SidebarProvider defaultOpen={false}>
+        <ModuleSideBar />
+        <main className="flex-1 min-w-0">
+          <SidebarTrigger triggerVariant="floating" />
+          {children}
+        </main>
+        <NodeSettingSidebar />
+      </SidebarProvider>
+    </div>
   );
 }

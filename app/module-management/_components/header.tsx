f3341f8c9bb631a5c8fc71da71beb5f629a5560e
Wendy Hu <yunhu412@gmail.com>
2026-01-07 18:46:19 +0800

    refactor(module-management): adjust header element to h2

--- app/module-management/_components/header.tsx
+++ app/module-management/_components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
           <Button className="border-none has-[>svg]:px-2 gray-800 bg-gray-100 hover:bg-white">
             <ArrowLeftIcon />
           </Button>
-          <h1 className="body-1 text-gray-900 px-2">模組管理</h1>
+          <h2 className="body-1 text-gray-900 px-2">模組管理</h2>
         </div>
         <div className="flex items-center gap-x-3 px-3">
           <Button className="text-sm/[1.5] bg-green-500 text-gray-100">

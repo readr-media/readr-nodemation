6b3fac79bebe778edf49bea5e1075cb4345edbc2
Wendy Hu <yunhu412@gmail.com>
2026-01-11 22:13:22 +0800

    feat(module-management): import and use ModuleDashboard and ModuleSectiocomponent

--- app/module-management/page.tsx
+++ app/module-management/page.tsx
@@ -1,3 +1,11 @@
+import ModuleDashboard from "./_components/module-dashboard";
+import ModuleSection from "./_components/module-section";
+
 export default function Page() {
-  return <div>hello</div>;
+  return (
+    <main className="flex flex-col py-10 px-15 gap-y-10 bg-gray-200 min-h-screen">
+      <ModuleDashboard />
+      <ModuleSection />
+    </main>
+  );
 }

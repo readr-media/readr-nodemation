30764d42119a0512922d51be38e96912a1113ae6
Wendy Hu <yunhu412@gmail.com>
2026-01-12 16:21:57 +0800

    feat(module-management): pass size props

--- app/module-management/_components/header.tsx
+++ app/module-management/_components/header.tsx
@@ -8,13 +8,13 @@ export default function Header() {
       <div className="flex justify-between items-center h-16 px-6 bg-gray-100 border-b border-b-[#e8e7e2]">
         <div className="flex items-center gap-x-2">
           <Button className="border-none has-[>svg]:px-2 gray-800 bg-gray-100 hover:bg-white">
-            <ArrowLeftIcon />
+            <ArrowLeftIcon size={16} />
           </Button>
           <h2 className="body-1 text-gray-900 px-2">模組管理</h2>
         </div>
         <div className="flex items-center gap-x-3 px-3">
           <Button className="text-sm/[1.5] bg-green-500 text-gray-100">
-            <PlusIcon />
+            <PlusIcon size={16} />
             新增模組
           </Button>
           <UserInfo />

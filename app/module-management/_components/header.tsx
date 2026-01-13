1540cdfd1a24a5616bdb688914263531d4f7384d
Wendy Hu <yunhu412@gmail.com>
2026-01-13 23:46:51 +0800

    feat(module-management): update background color and hover effect

--- app/module-management/_components/header.tsx
+++ app/module-management/_components/header.tsx
@@ -5,15 +5,15 @@ import { UserInfo } from "@/components/layout/user-info";
 export default function Header() {
   return (
     <header>
-      <div className="flex justify-between items-center h-16 px-6 bg-gray-100 border-b border-b-[#e8e7e2]">
+      <div className="flex justify-between items-center h-16 px-6 bg-white border-b border-b-border-b-gray-400">
         <div className="flex items-center gap-x-2">
-          <Button className="border-none has-[>svg]:px-2 gray-800 bg-gray-100 hover:bg-white">
+          <Button className="border-none has-[>svg]:px-2 hover:bg-gray-300">
             <ArrowLeftIcon size={16} />
           </Button>
           <h2 className="body-1 text-gray-900 px-2">模組管理</h2>
         </div>
         <div className="flex items-center gap-x-3 px-3">
-          <Button className="text-sm/[1.5] bg-green-500 text-gray-100">
+          <Button className="text-sm/[1.5] bg-green-500 text-white hover:bg-green-700">
             <PlusIcon size={16} />
             新增模組
           </Button>

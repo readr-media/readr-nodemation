6aa13cba27f84b0741ed2c172b04152f12521988
Wendy Hu <yunhu412@gmail.com>
2026-01-19 20:13:55 +0800

    style(module-management): update gap-y value

--- app/module-management/_components/new-module-pop-up.tsx
+++ app/module-management/_components/new-module-pop-up.tsx
@@ -23,6 +23,8 @@ import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 
 export default function NewModulePopUp() {
+  const DialogContentStyle =
+    "max-w-[512px] border border-gray-400 rounded-xl p-6 bg-gray-200 gap-y-5 [&_[data-slot=dialog-close]:hover]:bg-gray-400 [&_[data-slot=dialog-close]:hover]:ring-0 [&_[data-slot=dialog-close]:hover]:ring-offset-0 [&_[data-slot=dialog-close]]:cursor-pointer [&_[data-slot=dialog-close]]:p-2 [&_[data-slot=dialog-close]]:rounded-lg";
   const labelStyle = "title-6 text-gray-900 mb-2";
   const inputBasicStyle =
     "border border-gray-400 rounded-lg py-2 px-3 bg-white body-2 text-gray-900";
@@ -41,64 +43,66 @@ export default function NewModulePopUp() {
           </Button>
         </DialogTrigger>
 
-        <DialogContent className="max-w-[512px] border border-gray-400 rounded-xl p-6 bg-gray-200 gap-y-5 [&_[data-slot=dialog-close]:hover]:bg-gray-400 [&_[data-slot=dialog-close]:hover]:ring-0 [&_[data-slot=dialog-close]:hover]:ring-offset-0 [&_[data-slot=dialog-close]]:cursor-pointer [&_[data-slot=dialog-close]]:p-2 [&_[data-slot=dialog-close]]:rounded-lg">
+        <DialogContent className={DialogContentStyle}>
           <DialogHeader>
             <DialogTitle className="title-4 text-gray-900">
               新增模組
             </DialogTitle>
           </DialogHeader>
 
-          <div>
-            <Label htmlFor="模組名稱" className={labelStyle}>
-              模組名稱
-            </Label>
-            <Input
-              id="module-name"
-              name="模組名稱"
-              placeholder="請輸入名稱"
-              className={cn(inputBasicStyle, inputPseudoStyle)}
-            />
-          </div>
+          <div className="flex flex-col gap-y-3">
+            <div>
+              <Label htmlFor="模組名稱" className={labelStyle}>
+                模組名稱
+              </Label>
+              <Input
+                id="module-name"
+                name="模組名稱"
+                placeholder="請輸入名稱"
+                className={cn(inputBasicStyle, inputPseudoStyle)}
+              />
+            </div>
 
-          <div className="body-2">
-            <Label htmlFor="模組類型" className={labelStyle}>
-              模組類型
-            </Label>
-            <Select>
-              <SelectTrigger
-                className={cn(inputBasicStyle, selectTriggerStyle)}
-              >
-                <SelectValue placeholder="請選擇模組類型" />
-              </SelectTrigger>
+            <div className="body-2">
+              <Label htmlFor="模組類型" className={labelStyle}>
+                模組類型
+              </Label>
+              <Select>
+                <SelectTrigger
+                  className={cn(inputBasicStyle, selectTriggerStyle)}
+                >
+                  <SelectValue placeholder="請選擇模組類型" />
+                </SelectTrigger>
 
-              <SelectContent side="bottom" sideOffset={9}>
-                <SelectItem value="ai" className="cursor-pointer">
-                  AI 模組
-                </SelectItem>
-                <SelectItem value="code" className="cursor-pointer">
-                  程式碼模組
-                </SelectItem>
-                <SelectItem value="cms" className="cursor-pointer">
-                  CMS 模組
-                </SelectItem>
-                <SelectItem value="content" className="cursor-pointer">
-                  內容整理模組
-                </SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
+                <SelectContent side="bottom" sideOffset={9}>
+                  <SelectItem value="ai" className="cursor-pointer">
+                    AI 模組
+                  </SelectItem>
+                  <SelectItem value="code" className="cursor-pointer">
+                    程式碼模組
+                  </SelectItem>
+                  <SelectItem value="cms" className="cursor-pointer">
+                    CMS 模組
+                  </SelectItem>
+                  <SelectItem value="content" className="cursor-pointer">
+                    內容整理模組
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
 
-          <div>
-            <Label htmlFor="模組說明" className={labelStyle}>
-              模組說明（選填）
-            </Label>
-            <Input
-              id="module-description"
-              name="模組說明"
-              placeholder="請輸入內容"
-              defaultValue=""
-              className={cn(inputBasicStyle, inputPseudoStyle)}
-            />
+            <div>
+              <Label htmlFor="模組說明" className={labelStyle}>
+                模組說明（選填）
+              </Label>
+              <Input
+                id="module-description"
+                name="模組說明"
+                placeholder="請輸入內容"
+                defaultValue=""
+                className={cn(inputBasicStyle, inputPseudoStyle)}
+              />
+            </div>
           </div>
 
           <DialogFooter className="flex gap-x-3">

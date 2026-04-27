ccbeb02416fa55bfe3f987c6971a3796c2f16585
nihclil <oxx921@gmail.com>
2026-04-27 10:33:49 +0800

    fix: wrap module-management header with suspense

--- app/module-management/layout.tsx
+++ app/module-management/layout.tsx
@@ -1,9 +1,16 @@
 import Header from "./_components/header";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <Header />
+      <Suspense
+        fallback={
+          <div className="h-16 w-full bg-white border-b border-gray-400" />
+        }
+      >
+        <Header />
+      </Suspense>
       {children}
     </div>
   );
